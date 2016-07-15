240ac6d68af81cd8fa5bcc54d557d50deda32dc3
jeffryporter <jporter7@gmail.com>
2016-07-15 07:26:32 -0400

    Add aggravation

--- src/ants_clojure/core.clj
+++ src/ants_clojure/core.clj
@@ -4,30 +4,46 @@
 
 (def width 800)
 (def height 600)
-(def ant-count 100)
+(def ant-count 200)
 (def ants (atom []))
+(def min-distance 15)
 
 (defn create-ants []
   (for [i (range 0 ant-count)]
     {:x (rand-int width)
-     :y (rand-int height)}))
+     :y (rand-int height)
+     :color (javafx.scene.paint.Color/BLACK)}))
+
+
 
 (defn draw-ants! [context]
   (.clearRect context 0 0 width height)
   (doseq [ant @ants]
-    (.setFill context javafx.scene.paint.Color/BLACK)
+    (.setFill context (:color ant))
     (.fillOval context (:x ant) (:y ant) 5 5)))
 
 (defn move-ant [ant]
-  (Thread/sleep 1)
   (let [x-change (- (* 2 (rand)) 1)
         y-change (- (* 2 (rand)) 1)]
     (assoc ant 
       :x (+ (:x ant) x-change)
       :y (+ (:y ant) y-change))))
 
+
+(defn aggravate-ant [ant]
+  (let [nearby-ants (filter (fn [a]
+                              (and (< (Math/abs (- (:x ant) (:x a))) min-distance)
+                                   (< (Math/abs (- (:y ant) (:y a))) min-distance)))
+                      @ants)
+        nearby-count (count nearby-ants)]
+    (assoc ant
+      :color
+      (if (> nearby-count 1)
+        javafx.scene.paint.Color/RED 
+        javafx.scene.paint.Color/BLACK))))
+  
 (defn move-ants []
-  (pmap move-ant @ants))
+  (doall (pmap aggravate-ant (pmap move-ant @ants))))
 
 (def last-timestamp (atom 0))
 
@@ -47,7 +63,7 @@
                   (reset! ants (move-ants))
                   (draw-ants! context)
                   (.setText fps-label (str (fps now)))
-                  (reset! last-timestamp now)))]                  
+                  (reset! last-timestamp now)))]
     (.setTitle stage "Ants")
     (.setScene stage scene)
     (.show stage)
